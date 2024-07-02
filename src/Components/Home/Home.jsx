f2d9e8b372afe8f042c9157546c518aede20f9ee
Yousof Ali <yousofali293@gmail.com>
2024-07-03 02:05:31 +0600

    all done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import { Helmet } from "react-helmet-async";
 
-
 const Home = () => {
     return (
         <div>
             <Helmet>
                 <title>React map | Home</title>
             </Helmet>
+            
             <p className="text-center">i am from home!</p>
         </div>
     );
